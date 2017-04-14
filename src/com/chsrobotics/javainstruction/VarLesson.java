fd0a546d9fdc0d6ef8f2a7d83dfb8a1ae712891a
ADSERV\landryav <landryav@students.osd.wednet.edu>
2017-04-14 12:34:46 -0700

    no message

---
+++ src/com/chsrobotics/javainstruction/VarLesson.java
@@ -0,0 +1,29 @@
+package com.chsrobotics.javainstruction;
+
+public class VarLesson {
+int myVar1;
+myVar1 = 5; // fails, not in a method
+
+public void myMethod()
+{
+	int Var2;
+	final int myVar4 = 9;
+	
+	myVar2 = 1;
+	myVar1 = 1;
+	
+	if (myVar1 == 1)
+	{
+		int myVar3 = 2;
+		
+		myVar3 = myVar2 + myVar1		
+	}
+	myVar3 = 7;
+}
+ 
+public void myMethod2()
+
+{
+	myVar = 3;
+	}
+}

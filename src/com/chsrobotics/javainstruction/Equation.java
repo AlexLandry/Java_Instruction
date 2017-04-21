3ecfd102b40e40a3816910719e7038af8e06d140
ADSERV\landryav <landryav@students.osd.wednet.edu>
2017-04-21 12:47:43 -0700

    Completed Eqution

--- src/com/chsrobotics/javainstruction/Equation.java
+++ src/com/chsrobotics/javainstruction/Equation.java
@@ -1,18 +1,29 @@
 package com.chsrobotics.javainstruction;
 
 public class Equation {
-	
-	     public static void main(String []args)
-	     {
-	        myMethod();
-	     }
-
-	     public static void myMethod()
-	     {
-	    	 int x = 3;
-	    	 for ( x = 3 ; ;)
-	         if (x == 3) break;
-	         float y = (x+4)*(x-4)/x-3;
-	         System.out.println(x);
-	     }    
-	}
+ 	
+ 	     public static void main(String []args)
+ 	     {
+ 	        myMethod();
+ 	     }
+ 
+ 	     public static void myMethod()
+ 	     {
+ 	    	 for (int x = 0 ; x <= 3; x++)
+ 	    	 {
+ 	    		 float y = (x+4)*(x+4);
+ 	 	         float z = x-3;
+ 	 	         float a = y/z;
+ 	 	         System.out.println(a);
+ 	 	         }
+ 	    	 for (int x = 4 ; x <= 10; x++)
+ 	    	 {
+ 	    		 float y = (x+4)*(x+4);
+ 	 	         float z = x-3;
+ 	 	         float a = y/z;
+ 	 	         System.out.println(a);
+ 	    	 }
+ 	    	
+ 
+ 	     }    
+ 	} 
